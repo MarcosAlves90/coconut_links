9fe7bf7841065433744e9f232bf9988de979e616
MarcosAlves90 <MarcosAlves25@proton.me>
2025-09-17 03:15:05 -0300

    Comment out the base path configuration in Vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/coconut_links/' // Ajuste para o caminho do seu projeto no GitHub Pages
+  // base: '/coconut_links/'
 })
